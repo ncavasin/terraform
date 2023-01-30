b9f9ce1aba4c577e41cb3321e8f7b6429550f564
Nicolas Cavasin <ncavasin97@gmail.com>
2023-01-29 23:52:39 -0300

    add firewall rule to allow ICMP

--- gcp/02-vpcs/main.tf
+++ gcp/02-vpcs/main.tf
@@ -10,18 +10,26 @@ resource "google_compute_network" "custom_vpc" {
 }
 
 resource "google_compute_subnetwork" "subnetwork-east" {
-  ip_cidr_range = "10.1.0.0/24"
-  name          = "sub-east"
-  region        = "us-east1"
-  network       = google_compute_network.custom_vpc.id
+  ip_cidr_range            = "10.1.0.0/24"
+  name                     = "sub-east"
+  region                   = "us-east1"
+  network                  = google_compute_network.custom_vpc.id
   private_ip_google_access = true
 }
 
 resource "google_compute_subnetwork" "subnetwork-central" {
-  ip_cidr_range = "10.2.0.0/24"
-  name          = "sub-central"
-  region        = "us-central1"
-  network       = google_compute_network.custom_vpc.id
+  ip_cidr_range            = "10.2.0.0/24"
+  name                     = "sub-central"
+  region                   = "us-central1"
+  network                  = google_compute_network.custom_vpc.id
   private_ip_google_access = true
+}
 
+resource "google_compute_firewall" "allow_icmp" {
+  name    = "allow-icmp"
+  network = google_compute_network.custom_vpc.id
+  allow {
+    protocol = "ICMP"
+  }
+  source_ranges = ["181.170.109.131"]
 }
