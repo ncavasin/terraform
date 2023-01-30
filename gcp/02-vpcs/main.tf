bb986ee142623080250b1bdd3ec1ead2acc9b4b8
Nicolas Cavasin <ncavasin97@gmail.com>
2023-01-29 23:46:38 -0300

    add subnets

--- gcp/02-vpcs/main.tf
+++ gcp/02-vpcs/main.tf
@@ -1,10 +1,27 @@
-#resource "google_compute_network" "auto_vpc" {
-#  name                    = "auto-vpc"
-#  # Create a subnet in each region
-#  auto_create_subnetworks = true
-#}
+resource "google_compute_network" "auto_vpc" {
+  name                    = "auto-vpc"
+  # Create a subnet in each region
+  auto_create_subnetworks = true
+}
 
 resource "google_compute_network" "custom_vpc" {
   name                    = "custom-vpc"
   auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "subnetwork-east" {
+  ip_cidr_range = "10.1.0.0/24"
+  name          = "sub-east"
+  region        = "us-east1"
+  network       = google_compute_network.custom_vpc.id
+  private_ip_google_access = true
+}
+
+resource "google_compute_subnetwork" "subnetwork-central" {
+  ip_cidr_range = "10.2.0.0/24"
+  name          = "sub-central"
+  region        = "us-central1"
+  network       = google_compute_network.custom_vpc.id
+  private_ip_google_access = true
+
 }
